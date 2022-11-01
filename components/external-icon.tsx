57c82f94265fdde69de4722b4478a7a78782139e
Iker-Romero <ikersoftdev@gmail.com>
2022-11-02 00:30:32 +0100

    footer fix

--- components/external-icon.tsx
+++ components/external-icon.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
 const ExternalIcon = (props: { iconURL: string; externalURL: string; id: string }) => {
   const { iconURL, externalURL, id } = props;
 
   return (
-    <Link to={externalURL}>
+    <a href={externalURL} target="_blank" rel="noreferrer">
       <img src={iconURL} alt="External link icon" id={id} />
-    </Link>
+    </a>
   );
 };
 
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import ExternalIcon from './external-icon';
 
 const Footer = () => {
