ff55c72cce4160b8a1f04c315731587b928ee4a5
Iker-Romero <ikersoftdev@gmail.com>
2022-11-03 16:55:39 +0100

    show details page styled

--- app/src/routes/show-details.tsx
+++ app/src/routes/show-details.tsx
@@ -22,23 +22,23 @@ const ShowDetails = () => {
     const { Title, Year, Runtime, Genre, Director, Plot, Poster, Type, BoxOffice } = show;
 
     return (
-      <>
-        <div className="show-identification">
-          <h1>{Title}</h1>
-          <h2>({Year})</h2>
+      <div className="show-details">
+        <h1>{Title}</h1>
+        <h2>({Year})</h2>
+        <div>
+          <img src={Poster} alt={Title} />
+          <div className="details-container">
+            <p>{Plot}</p>
+            <ul>
+              <li>{Runtime}</li>
+              <li>{Genre}</li>
+              <li>{Director}</li>
+              <li>{Type}</li>
+              <li>{BoxOffice}</li>
+            </ul>
+          </div>
         </div>
-        <img src={Poster} alt={Title} />
-        <div className="show-info">
-          <p>{Plot}</p>
-          <ul>
-            <li>{Runtime}</li>
-            <li>{Genre}</li>
-            <li>{Director}</li>
-            <li>{Type}</li>
-            <li>{BoxOffice}</li>
-          </ul>
-        </div>
-      </>
+      </div>
     );
   }
   return <h1>Loading...</h1>;
